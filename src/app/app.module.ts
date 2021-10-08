19288fcf3e17d3c708302b73da89a0fbd0566d10
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-10-09 01:34:25 +0300

    Adds image to page and creates authors component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     QuotesComponent,
-    NavBarComponent
+    NavBarComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
