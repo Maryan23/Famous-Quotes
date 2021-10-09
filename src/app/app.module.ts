1f1f77a0c2c8331a30888cc37cea0f33760359bd
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-10-09 13:51:09 +0300

    Add contact component and style app component sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './about/about.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AuthorsComponent } from './authors/authors.component';
     AboutComponent,
     QuotesFormComponent,
     NavBarComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
