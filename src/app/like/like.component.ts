703498294620e19051abb6bfa34359ae46663dd8
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-10-12 15:11:12 +0300

    Style like and dislike button and add logic to them

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-like',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
+  numberOfLikes:number =0;
+
+  likeButtonClick(){
+    this.numberOfLikes++;
+
+  }
+  dislikeButtonClick(){
+    this.numberOfLikes--;
+
+  }
 
   constructor() { }
 
